760900a93798ccc8da78a3b783221f1eecaf599b
xh3b4sd <xh3b4sd@gmail.com>
2023-09-18 17:07:48 +0200

    make searchRecord a cursor based iterator (#8)

--- contracts/Policy.sol
+++ contracts/Policy.sol
@@ -39,7 +39,13 @@ contract Policy {
     /// TYPES
     ///
 
-    /// @notice TODO
+    /// @notice _amount is the maximum amount of records returned in a single call to searchRecord.
+    uint256 private _amount;
+
+    /// @notice _blocks is the block height of the last internal state change.
+    uint256 private _blocks;
+
+    /// @notice _states is the internally managed list of all Records onchain.
     Triple.States private _states;
 
     ///
@@ -49,7 +55,17 @@ contract Policy {
     /// @notice constructor for initializing an instance of the Policy contract.
     /// @notice Creates first record for the deployer address with access zero in system zero.
     /// @notice Emits Created for the added record.
-    constructor() {
+    /// @param amo the optional maximum amount of records returned in a single call to searchRecord.
+    /// @param amo must be greater than 0 and smaller or equal to 1000.
+    /// @param amo defaults to 100 otherwise.
+    constructor(uint256 amo) {
+        if (amo > 0 && amo <= 1000) {
+            _amount = amo;
+        } else {
+            _amount = 100;
+        }
+
+        _blocks = block.number;
         _states._createRecord(Triple.Record({sys: 0, mem: msg.sender, acc: 0}));
         emit Created(0, msg.sender, 0);
     }
@@ -61,12 +77,14 @@ contract Policy {
     /// @notice createRecord allows privileged members to add records onchain.
     /// @notice Reverts on unprivileged access.
     /// @notice Emits Created for the added record.
+    /// @param rec the record to create.
     function createRecord(Triple.Record memory rec) public {
         if (!_verifyCreate(rec)) {
             revert();
         }
 
         {
+            _blocks = block.number;
             _states._createRecord(rec);
             emit Created(rec.sys, rec.mem, rec.acc);
         }
@@ -75,12 +93,14 @@ contract Policy {
     /// @notice deleteRecord allows privileged members to remove records onchain.
     /// @notice Reverts on unprivileged access.
     /// @notice Emits Deleted for the removed record.
+    /// @param rec the record to delete.
     function deleteRecord(Triple.Record memory rec) public {
         if (!_verifyDelete(rec)) {
             revert();
         }
 
         {
+            _blocks = block.number;
             _states._deleteRecord(rec);
             emit Deleted(rec.sys, rec.mem, rec.acc);
         }
@@ -90,9 +110,46 @@ contract Policy {
     /// VIEW
     ///
 
-    /// @notice searchRecord returns all records managed internally.
-    function searchRecord() public view returns (Triple.Record[] memory) {
-        return _states._searchRecord();
+    /// @notice searchAmount returns the maximum amount of records returned in a single call to searchRecord.
+    function searchAmount() public view returns (uint256) {
+        return _amount;
+    }
+
+    /// @notice searchBlocks returns the block height of the last internal state change.
+    function searchBlocks() public view returns (uint256) {
+        return _blocks;
+    }
+
+    /// @notice searchRecord returns all records of a cursor based iteration.
+    /// @notice Iteration results span from cur to cur + _amount.
+    /// @notice searchBlocks must be called before the first call to searchRecord.
+    /// @notice searchBlocks must be called after the last call to searchRecord.
+    /// @notice The search results are valid if the block height did not change between the first and last call to searchRecord.
+    /// @notice Reverts if blo != _blocks.
+    /// @param cur the cursor to start iterating from.
+    /// @param blo the expected block height required to match _blocks.
+    /// @return cur the new cursor for consecutive calls, or 0 if done.
+    /// @return rec the list of records according to cur and _amount.
+    function searchRecord(
+        uint256 cur,
+        uint256 blo
+    ) public view returns (uint256, Triple.Record[] memory) {
+        if (blo != _blocks) {
+            revert();
+        }
+
+        (bool don, Triple.Record[] memory rec) = _states._searchRecord(
+            cur,
+            cur + _amount
+        );
+
+        if (don) {
+            cur = 0;
+        } else {
+            cur = cur + _amount;
+        }
+
+        return (cur, rec);
     }
 
     ///
@@ -142,6 +199,7 @@ contract Policy {
         //     The member to be added must become access zero.
         //
         if (!dse && czz && maz) {
+            // create system
             return true;
         }
 
@@ -155,6 +213,7 @@ contract Policy {
         //     The caller must not add themselves.
         //
         if (dse && !dme && dce && chp && !cim) {
+            // add member
             return true;
         }
 
@@ -166,6 +225,7 @@ contract Policy {
         //     The caller must add themselves.
         //
         if (dse && czz && !dme && cim) {
+            // add member
             return true;
         }
 
@@ -208,10 +268,10 @@ contract Policy {
         // msg.caller might be gaining access zero.
         bool chp = _states._searchAccess(rec.sys, msg.sender) <= rec.acc;
 
-        // maz, multiple access zero, expresses whether the given system to
-        // remove the given member from has multiple access zero members. Must
-        // be used together with dse (does system exist). Otherwise an empty
-        // member count for the given access will be used.
+        // maz, many access zero, expresses whether the given system to remove
+        // the given member from has many access zero members. Must be used
+        // together with dse (does system exist). Otherwise an empty member
+        // count for the given access will be used.
         bool maz = _states._searchAccess(rec.sys, 0) > 1;
 
         // saz, system access zero, expresses whether the given member has
@@ -224,13 +284,15 @@ contract Policy {
         // member.
         bool oom = _states._searchMember(rec.sys) == 1;
 
-        // Anyone can remove themselves.
+        // Anyone can remove themselves, if they do not have access zero.
         //
         //     The record to remove must exist as given.
         //     The caller must not have access zero in system zero.
+        //     The caller must not have access zero in the given system.
         //     The caller must remove themselves.
         //
-        if (dre && !czz && cim) {
+        if (dre && !czz && !saz && cim) {
+            // remove member
             return true;
         }
 
@@ -245,6 +307,7 @@ contract Policy {
         //     The caller must not remove themselves.
         //
         if (dre && dse && dme && dce && chp && !cim) {
+            // remove member
             return true;
         }
 
@@ -257,26 +320,13 @@ contract Policy {
         //     The caller must not remove themselves.
         //
         if (dre && dse && dme && czz && !cim) {
-            return true;
-        }
-
-        // Access zero in system zero can remove themselves from any system that
-        // is not system zero and delete that system in the process.
-        //
-        //     The record to remove must exist as given.
-        //     The system to remove the member from must exist.
-        //     The member to remove must exist in that system.
-        //     The caller must have access zero in system zero.
-        //     The caller must be the last member in that system.
-        //     The system to remove the member from must not be system zero.
-        //     The caller must remove themselves.
-        //
-        if (dre && dse && dme && czz && oom && rec.sys != 0 && cim) {
+            // remove member
             return true;
         }
 
         // Access zero in any given system can be removed from that system by
-        // access zero, if that system has many access zero members.
+        // access zero, if that system has many access zero members. Removal can
+        // happen by others, or be self serving.
         //
         //     The record to remove must exist as given.
         //     The system to remove the member from must exist.
@@ -286,6 +336,24 @@ contract Policy {
         //     The caller must have equal or higher access in that system.
         //
         if (dre && dse && dme && saz && maz && chp) {
+            // remove member
+            return true;
+        }
+
+        // Access zero in any given system can remove themselves from that
+        // system and delete that system in the process, if that system is not
+        // system zero.
+        //
+        //     The record to remove must exist as given.
+        //     The system to remove the member from must exist.
+        //     The member to remove must exist in that system.
+        //     The caller must have access zero in that system.
+        //     The caller must be the last member in that system.
+        //     The system to remove the member from must not be system zero.
+        //     The caller must remove themselves.
+        //
+        if (dre && dse && dme && saz && oom && rec.sys != 0 && cim) {
+            // delete system
             return true;
         }
 
