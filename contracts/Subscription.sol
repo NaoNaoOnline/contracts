9de5bd5fa5c82349617203502e4358ab56fa3f36
xh3b4sd <xh3b4sd@gmail.com>
2023-11-20 20:13:49 +0100

    fix

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 /// @title Subscription Management
 /// @author xh3b4sd
