522411f677fd75c1a7187298e556d4c2facdfcc4
xh3b4sd <xh3b4sd@gmail.com>
2023-11-21 14:32:33 +0100

    fix

--- test/Subscription.builtin.test.ts
+++ test/Subscription.builtin.test.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe.only("Subscription.setSubAmn", () => {
+describe("Subscription.setSubAmn", () => {
   const deployContract = async () => {
     const sig = await ethers.getSigners();
 
